44534fb07fab0af90fb8d47751d335d4c77b576b
raikox <jkevin.mv@gmail.com>
2019-12-04 02:50:58 -0500

    GetLineaInvestigacionArea(idAreaInvestigacion)

---
+++ AsesoriaTesisWebAPICore/AsesoriaTesisWebAPI/DataAccess/LineaInvestigacionDA.cs
@@ -0,0 +1,48 @@
+﻿using AsesoriaTesisWebAPI.CustomModels;
+using AsesoriaTesisWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsesoriaTesisWebAPI.DataAccess
+{
+    public class LineaInvestigacionDA
+    {
+        private readonly TutoriaContext dbContext;
+
+        public LineaInvestigacionDA()
+        {
+            dbContext = new TutoriaContext();
+        }
+
+        public async Task<ActionResult<IEnumerable<LineaAreaInvestigacion>>> GetLineaInvestigacionArea(int idAreaInvestigacion)
+        {
+            List<LineaAreaInvestigacion> listalineaAreaInvestigacions = new List<LineaAreaInvestigacion>();
+            var resultList = await dbContext.LineaInvestigacion
+                .Include(e => e.AreaInvestigacion)
+                .Where(e => e.AreaInvestigacionId.Equals(idAreaInvestigacion))
+                .ToListAsync();
+
+            foreach (var lista in resultList)
+            {
+                LineaAreaInvestigacion lineaAreaInvestigacion = new LineaAreaInvestigacion()
+                {
+                    LineaInvestigacionId = lista.LineaInvestigacionId,
+                    LineaInvestigacionNombre = lista.Nombre,
+                    LineaInvestigacionDescripcion = lista.Descripcion,
+                    AreaInvestigacionId = lista.AreaInvestigacion.AreaInvestigacionId,
+                    AreaInvestigacionNombre = lista.AreaInvestigacion.Nombre,
+                    AreaInvestigacionDescripcion = lista.AreaInvestigacion.Descripcion
+                };
+
+                listalineaAreaInvestigacions.Add(lineaAreaInvestigacion);
+            }
+
+            return listalineaAreaInvestigacions;
+
+        }
+
+    }
+}
